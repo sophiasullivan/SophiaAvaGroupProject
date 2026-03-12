ea1765c3a7ef85c06dde99545e7f1b4ad1389ea3
Sophia Sullivan <ssulli39@calpoly.edu>
2026-03-11 20:37:28 -0700

    sophia and ava group project

--- analyis.py
+++ analyis.py
@@ -1,11 +1,49 @@
 # Analysis Final Project CSC101
 #
-# Our initial thought is that our program could be used to gather insights into different demographics’ experiences with mathematics. This information could help educators improve the learning experience and better tailor math instruction to students’ needs. By analyzing these apatterns, we can make more informed observations and assumptions about how students interact with mathematics.
-# One analysis examined differences in math attitudes between males and females.When comparing the mean math attitudes between Cal Poly boys versus Cal Poly girls, the males had a higher average math attitude score (0.38) compared to the females (0.16). This observation suggests men report higher confidence or more positive attitudes toward mathematics than women but this does not reflect ability. However, it could reflect confidence gaps or social influences that shape how students perceive their own math skills. If women are less confident despite having similar abilities, educators could reduce this confidence gap by increasing the representation of women in STEM fields and by providing early confidence building exercises in mathematics when young. Understanding these differences can help educators design classroom environments that encourage equal participation and confidence for both genders.
-# This case specifically analyzes differences in math attitudes between academic colleges. Cal Poly College of Engineering students' average math attitudes were much higher than students from Cal Poly College of Liberal Arts (0.33>-0.29). This was an expected result since CENG students’ coursework relies heavily on mathematics. Students who already have a high confidence in mathematics are also more likely to choose a math-heavy major. These results reflect how attitudes toward math can influence people’s academic paths and decisions. Something beneficial that could come out of this specific analysis would be possibly raising awareness of real-world applications of math that aren’t just STEM related fields. Also adding additional math support classes for non-STEM majors would be helpful and thankfully, Cal Poly already provides that.
-# When comparing the standard deviation math attitudes of Asian Cal Poly students versus Hispanic/Latinx Cal Poly students, we found that the Asian students’ standard deviation math attitude (0.4) is greater than the Hispanic/Latinx students’ standard deviation math attitude (0.37). A higher standard deviation indicates more variation in attitudes within the group so Asian Cal Poly students had a wider-spread of math confidence levels. This shows that demographic groups are not uniform in their math experiences. Each student is different, having their own background, learning experiences and environments, and levels of academic support. This is an important insight because it tells educators to focus on individual learning needs and to avoid stereotypes.
-# This case studies changes in math attitudes over time, comparing 5th grade girls in the Journal of Child Development study to female Cal Poly students in our survey findings. We found that the women's standard deviation math attitude (0.4) is greater than Grade 5 Girls’ standard deviation math attitude (-0.17). These results show that math attitudes change significantly throughout one’s education. As students grow, they may gain more varied experiences, good or bad, that may increase their confidence or negatively impact it. Addressing negative math experiences in younger students before it discourages them to continue in those fields could be very beneficial in shaping how younger students mindsets who may find mathematics and other STEM related subjects challenging. This would also help in increasing and maintaining positive math experiences and attitudes throughout students’ education.
-# In conclusion, this program can help schools better understand confidence gaps between student groups, helps educators identify which students might need additional support, and helps educators broaden participation in STEM. Some limitations would be that our survey sample size is small and self reported attitudes may not reflect true math ability.
+# Our initial thought is that our program could be used to gather insights into different demographics’ experiences with
+# mathematics. This information could help educators improve the learning experience and better tailor math instruction to
+# students’ needs. By analyzing these patterns, we can make more informed observations and assumptions about how students
+# interact with mathematics.
+#
+# The first two testcases we wrote in order to inspect the data we collected were about mean math attitudes.
+# One analysis examined differences in math attitudes between males and females. When comparing the mean math attitudes
+# between Cal Poly boys versus Cal Poly girls, the males had a higher average math attitude score (0.38) compared to the
+# females (0.16). This observation suggests men report higher confidence or more positive attitudes toward mathematics
+# than women but this does not reflect ability. However, it could reflect confidence gaps or social influences that
+# shape how students perceive their own math skills. If women are less confident despite having similar abilities,
+# educators could reduce this confidence gap by increasing the representation of women in STEM fields and by providing
+# early confidence building exercises in mathematics when young. Understanding these differences can help educators design
+# classroom environments that encourage equal participation and confidence for both genders.
+#
+# The second testcase we wrote analyzes differences in math attitudes between academic colleges. Cal Poly College of
+# Engineering students' average math attitude was much higher than students from the College of Liberal Arts (0.33 > -0.29).
+# This was an expected result since CENG students’ coursework relies heavily on mathematics. Students who already have a
+# high confidence in mathematics are also more likely to choose a math-heavy major. These results reflect how attitudes
+# toward math can influence people’s academic paths and decisions. Something beneficial that could come out of this specific
+# analysis would be possibly raising awareness of real-world applications of math that aren’t just STEM related fields.
+# Also adding additional math support classes for non-STEM majors would be helpful and thankfully, Cal Poly already provides that.
+#
+# After comparing means, we wrote two more test cases, which compared demographic groups' standard deviations.
+# When comparing the standard deviation math attitudes of Asian Cal Poly students versus Hispanic/Latinx Cal Poly students,
+# we found that the Asian students’ standard deviation math attitude (0.4) is greater than the Hispanic/Latinx students’
+# standard deviation math attitude (0.37). A higher standard deviation indicates more variation in attitudes within the
+# group so Asian Cal Poly students had a wider-spread of math confidence levels. This shows that demographic groups are
+# not uniform in their math experiences. Each student is different, having their own background, learning experiences and
+# environments, and levels of academic support. This is an important insight because it tells educators to focus on
+# individual learning needs and to avoid stereotypes.
+
+# Our final case studies changes in math attitudes over time, comparing 5th grade girls in the Journal of Child Development
+# study to female Cal Poly students in our survey findings. We found that the women's standard deviation math attitude
+# (0.4) is greater than Grade 5 Girls’ standard deviation math attitude (-0.17). These results show that math attitudes
+# change significantly throughout one’s education. As students grow, they may gain more varied experiences, good or bad,
+# that may increase their confidence or negatively impact it. Addressing negative math experiences in younger students
+# before it discourages them to continue in those fields could be very beneficial in shaping how younger students mindsets
+# who may find mathematics and other STEM related subjects challenging. This would also help in increasing and maintaining
+# positive math experiences and attitudes throughout students’ education.
+#
+# In conclusion, this program can help schools better understand confidence gaps between student groups, helps educators
+# identify which students might need additional support, and helps educators broaden participation in STEM. Some limitations
+# would be that our survey sample size is small and self reported attitudes may not reflect true math ability.
 #
 # 
 #
